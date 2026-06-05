a7730fcf01d6fb7fdf62d81c1e9992287447a4b6
ShuklaBhanu <108919393+ShuklaBhanu@users.noreply.github.com>
2026-06-05 12:55:01 +0530

    Update index.js remove cursor

--- js/index.js
+++ js/index.js
@@ -1,15 +1,7 @@
 // Custom cursor
 const cursor = document.getElementById("cursor");
 const ring = document.getElementById("cursorRing");
-let mx = 0,
-    my = 0,
-    rx = 0,
-    ry = 0;
-document.addEventListener("mousemove", (e) => {
-    mx = e.clientX;
-    my = e.clientY;
-    cursor.style.transform = `translate(${mx - 4}px, ${my - 4}px)`;
-});
+
 (function animateRing() {
     rx += (mx - rx) * 0.11;
     ry += (my - ry) * 0.11;
